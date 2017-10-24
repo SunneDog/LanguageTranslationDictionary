ad355eaf2f8108767a56fc7cceb8de5b00bb45ed
Danni Sunne <etnichols55@gmai.com>
2017-10-24 10:50:01 -0700

    Moved all Files to FileManager

--- src/program/Dictionary.java
+++ src/program/Dictionary.java
@@ -1,11 +1,19 @@
 package program;
 
 import java.util.HashMap;
-import java.io.File;
 
 public class Dictionary {
     String dictName;
     HashMap<String, String> a2b;
-    File dictFile; // where the dictionary is stored + loaded from
+    String filePath; // where the dictionary is stored + loaded from
 
+    public Dictionary(String dictName, String filePath) {
+        this.dictName = dictName;
+        this.filePath = filePath;
+        // check if dict/file combo already exists
+
+        // if exists, add data from file to HashMap
+
+        // else create new file for new dict
+    }
 }
--- src/program/FileManager.java
+++ src/program/FileManager.java
@@ -5,8 +5,11 @@ import java.io.File;
 // manages files for Main and Dictionary
 public class FileManager {
     File myFile;
+    String type; // OPTIONS: dict,text
+    // determines which read/write methods are used
 
-    public FileManager(String fileName) { // fileName requires entire relative path
+    public FileManager(String fileName, String type) { // fileName requires entire relative path
         myFile = new File(fileName);
+        this.type = type;
     }
 }
